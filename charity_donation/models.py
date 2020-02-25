1d9e17f2e9e7687a53e40fd852f3b03354c6df0a
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2020-02-25 17:32:17 +0100

    creating models and user application

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+TYPES = (
+    (1, "fundacja"),
+    (2, "organizacja pozarządowa"),
+    (3, "zbiórka lokalna"),
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.CharField(choices=TYPES, default=1)
+    categories = models.ManyToManyField("Category")
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField("Category")
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=120)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=120)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateTimeField()
+    pick_up_comment = models.TextField()
--- portfolio_lab/settings.py
+++ portfolio_lab/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'charity_donation.apps.CharityDonationConfig',
+    'user',
 ]
 
 MIDDLEWARE = [
