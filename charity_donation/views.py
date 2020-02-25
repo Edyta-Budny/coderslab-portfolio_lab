bb249648fbf894affc7e212f306a42e5d849900a
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2020-02-25 16:59:19 +0100

    preparation of templates

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, "html/index.html")
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, "html/form.html")
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "html/login.html")
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "html/register.html")
--- portfolio_lab/urls.py
+++ portfolio_lab/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
+from charity_donation.views import LandingPageView, AddDonationView, LoginView, RegisterView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view()),
+    path('add_donation/', AddDonationView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
 ]
+
