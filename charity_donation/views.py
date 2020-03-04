5b170fd098bd9891af96dc1d44066eb804b713ef
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2020-03-04 12:38:03 +0100

    modification LandingPageView

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,4 +1,3 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.views import View
 from charity_donation.models import Donation, Institution
@@ -9,14 +8,20 @@ class LandingPageView(View):
     def get(self, request):
         sack_counter = Donation.objects.aggregate(sum_q=Sum('quantity'))["sum_q"]
         supported_institution = Institution.objects.annotate(count_d=Count('donation')).filter(count_d__gte=1).count()
-        foundation_list = Institution.objects.filter(type="F")
-        organization_list = Institution.objects.filter(type="NO")
-        collection_list = Institution.objects.filter(type="LC")
-        return render(request, "html/index.html", {"sack_counter": sack_counter,
-                                                   "supported_institution_counter": supported_institution,
-                                                   "foundations": foundation_list,
-                                                   "organizations": organization_list,
-                                                   "collections": collection_list})
+        foundation_list = Institution.objects.filter(type=Institution.FOUNDATION)
+        organization_list = Institution.objects.filter(type=Institution.NONGOVERNMENTAL_ORGANIZATION)
+        collection_list = Institution.objects.filter(type=Institution.LOCAL_COLLECTION)
+        return render(
+            request,
+            "html/index.html",
+            {
+                "sack_counter": sack_counter,
+                "supported_institution_counter": supported_institution,
+                "foundations": foundation_list,
+                "organizations": organization_list,
+                "collections": collection_list
+            }
+        )
 
 
 class AddDonationView(View):
