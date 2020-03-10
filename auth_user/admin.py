a3ac1e5a577c504d0fa8f5edbd2236da784d9242
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2020-03-10 18:18:22 +0100

    update administration panel

--- auth_user/admin.py
+++ auth_user/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from auth_user.models import User
+from charity_donation.models import Institution, Donation, Category
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Category)
+admin.site.register(Institution)
+admin.site.register(Donation)
